2c95e7e5dd6bcaf9db8130919c71d1671f670b82
chpatinos <ch.patino@uniandes.edu.co>
2020-11-17 03:36:37 -0500

    Backend Fix

--- BACKEND/models/index.js
+++ BACKEND/models/index.js
@@ -1,17 +1,22 @@
 const Sequelize = require("sequelize");
 const dotenv = require("dotenv");
 
+let URI;
+
 dotenv.config({
   path: "./config.env",
 });
 
-const sequelize = new Sequelize(process.env.DB_URI, { logging: false });
+if (process.env.NODE_ENV === "production") URI = process.env.DB_URI_PRODUCTION;
+if (process.env.NODE_ENV === "test") URI = process.env.DB_URI_TESTING;
+
+const sequelize = new Sequelize(URI, { logging: false });
 
 const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.persons = require("./personModel")(sequelize, Sequelize)
+db.persons = require("./personModel")(sequelize, Sequelize);
 
 module.exports = db;
